7d9ba0924940aa7fd6363b4b4b6db6e9fcfd10bd
Ivanov22@gmail.com <kngk_github@mail.ru>
2024-04-06 07:59:48 +0300

    Добавьте файлы проекта.

---
+++ model/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RandomNumberGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(1, 101); // Генерация случайного числа от 1 до 100
+
+            Console.WriteLine($"Случайное число: {randomNumber}");
+        }
+    }
+}
